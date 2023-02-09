6cf7e3666b2fd6eefdcb6668ac5a65f3ca45315c
Allison Sobol <allisonsobol13@gmail.com>
2023-02-08 16:27:01 -0800

    certifications

--- js/index.js
+++ js/index.js
@@ -5,3 +5,8 @@ const navbarMenu = document.querySelector('#nav-links');
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
 });
+
+// overlay
+function myFunction() {
+    document.getElementById("cert1").value = document.getElementById("overlay").value;
+}
