1da9035f9db74a754b7899758fb2ae6abf365b78
Allison Sobol <allisonsobol13@gmail.com>
2023-08-04 18:33:12 -0700

    Working on styling

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// burger menu for mobile
+// Burger menu for mobile
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 
@@ -6,10 +6,22 @@ burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
 });
 
-// overlay
+// Overlay for Certifications
 function showOverlay() {
     document.getElementById("overlay").style.display = "inline-block";
 }
     function hideOverlay() {
         document.getElementById("overlay").style.display = "none";
     }
+
+// Scroll Reveal for about page
+const sr = ScrollReveal();
+
+sr.reveal('.element-item', {
+  origin: 'bottom',
+  distance: '20px',
+  duration: 1000,
+  delay: 0,
+  opacity: 0,
+  scale: 1,
+});
